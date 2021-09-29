0170ca7ac66f86eabb29d5b199f4e83264a2b44c
l-monninger <liammmonninger@gmail.com>
2021-09-28 19:50:11 -0700

    Updating the build.

--- dist/cjs/index.js
+++ dist/cjs/index.js
@@ -11,3 +11,4 @@ var __exportStar = (this && this.__exportStar) || function(m, exports) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 __exportStar(require("./Rgfm"), exports);
+__exportStar(require("./__test__"), exports);
--- dist/mjs/index.d.ts
+++ dist/mjs/index.d.ts
@@ -1,1 +1,2 @@
 export * from "./Rgfm";
+export * from "./__test__";
